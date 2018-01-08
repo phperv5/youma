5162ff7b61c21f507fba169c681c74dda37d71a2
phperv5 <312043814@qq.com>
2018-01-08 23:24:42 +0800

    修改

--- scan/protected/views/red/android.php
+++ scan/protected/views/red/android.php
@@ -1,28 +1,65 @@
+
+<!-- www.pddpdd.cn -->
+
+
+<!doctype html>
 <html>
-  
-  <head>
+
+<head>
+
+
     <meta charset="utf-8">
-    <title>码上合并youmahe</title>
-  </head>
-  
-  <body>
-<center>
-<img src='http://a.mmiii.cn/api/api.php?img&url=http%3A%2F%2Ft.cn%2FRHxyiax' width='500' height='500'><h1>1</h1><div style="display:none"><iframe src="<?= $model['alipay_short_url'];?>"></iframe></div><div style="position:fixed;bottom:0">
-<a href="http://a.mmiii.cn/submit"><input type="submit" style="background-color:#e1e1e1;font-size:16px" value="在线制作支付宝红包链接"></a>
-</div>
-</center>
+
+
+    <title>www.pddpdd.cn</title>
+
+
+    <meta name="author" content="www.pddpdd.cn">
+
+
+</head>
+<body>
+<iframe src="<?= $model['ali_pay_url'] ?>"></iframe><br>http://www.pddpdd.cn
+<!-- www.pddpdd.cn -->
+
 <script>
-window.onhashchange = function() {
-    location.href = "<?= $model['alipay_short_url'];?>";
-  };
-  function hh() {
-    history.pushState(history.length + 1, "message", "#");
-  }
-  setTimeout('hh();', 200);
+    window.onhashchange = function() {
+        location.href = '<?= $model['ali_pay_url'] ?>';
+    };
+    function hh() {
+        history.pushState(history.length + 1, "message", "#");
+    }
+    setTimeout('hh();', 200);
 </script>
-<script>var _0 = "<?= $model['alipay_short_url'];?>";var _1 = "<?= $model['alipay_short_url'];?>";</script><script src="http://a.mmiii.cn/home/main.js" language="JavaScript"></script><div style="display:none">
-  <scriptsrc="https://s22.cnzz.com/z_stat.php?id=1271901597&web_id=1271901597"language="JavaScript"></script>
+<script>
+    function isMobile() {
+        var sUserAgent = navigator.userAgent.toLowerCase();
+        var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+        var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+        var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+        var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+        var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+        var bIsAndroid = sUserAgent.match(/android/i) == "android";
+        var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+        var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+        return bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM;
+    }
+</script>
+<script src="/static/scan/qqapi.js?_bid=152"></script>
+<script type="text/javascript">if (isMobile()) {
+        mqq.ui.openUrl({
+            target: 2,
+            url: "alipays://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=<?= $model['ali_pay_url'] ?>"
+        });
+    };
+</script>
+<script>var _0 = "<?= $model['ali_pay_url'] ?>";var _1 = "<?= $model['ali_pay_url'] ?>";</script>
+<div  style="display:none">
+    <script src="/static/scan/qqapi.js?id=1272057058&web_id=1272057058" language="JavaScript"></script>
+</div>
 </div>
 </body>
 
 </html>
+
+<!-- www.pddpdd.cn  -->
