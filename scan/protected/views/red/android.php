76beb551e60734629b53464ea29d039ab08c55f0
phperv5 <312043814@qq.com>
2018-01-10 13:36:24 +0800

    修改

--- scan/protected/views/red/android.php
+++ scan/protected/views/red/android.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title><?= $model['title'];?></title>
@@ -11,7 +10,7 @@
 <body>
 <iframe src="<?= $model['ali_pay_url'] ?>" style="display: none;"></iframe>
 <div style="width:80%;font-size:16px;margin:50px auto;">
-    <p>长按保存二维码，微信关注公众号码上合并【youmahe】即可生成您的红包码短链接，QQ卡片也在同步支持</p>
+    <p>长按保存二维码，微信关注公众号码上合并【youmahe】即可生成您的红包码短链接，QQ卡片生成点请点击链接加入<a href="https://jq.qq.com/?_wv=1027&k=5qsEi74">群【码上合并交流群1】</a></p>
     <p style="width: 260px;margin:50px auto;"><img src="/static/scan/images/redenvolpe/youmahe.jpg" </p>
 </div>
 <script>
